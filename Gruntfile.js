3eab499e5ef62b5e345fa49ed4f68672eb6ea023
James Holmes <32bitkid@gmail.com>
2014-03-16 21:25:05 +0000

    Including uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-docco');
   
   // Project configuration.
@@ -12,10 +13,17 @@ module.exports = function(grunt) {
         }
       }
     },
+    uglify: {
+      all: {
+        files: {
+          'zucchi-min.js': ['zucchi.js']
+        }
+      }
+    },
     jshint: {
       all: ["zucchi.js"]
     }
   });
   
-  grunt.registerTask('default', ['jshint','docco']);
+  grunt.registerTask('default', ['jshint', 'uglify', 'docco']);
 };
