394ebf1642d0578353b700891b80852d62e8f25d
James Holmes <32bitkid@gmail.com>
2014-03-16 21:17:00 +0000

    Updating pages

--- zucchi.js
+++ zucchi.js
@@ -1,16 +1,43 @@
 ;(function(ctx) {
   
-  var def = typeof(define) === "function"
-    ?  define
-    : typeof(module) !== "undefined"
-    ? function(m) { module.exports = m(); }
+  var def = typeof(define) === "function" ? define
+    : typeof(module) !== "undefined" ? function(m) { module.exports = m(); }
     : function(m) { ctx.zucchi = m(); };
 
-
+  // Reporters 
+  var reporters = {
+    silent: { 
+      onSuccess: function() { },
+      onFailure: function() { },
+      exceptionFormatter: function(ex) { return ex.stack; },
+      onResults: function(p, f, failures) { throw failures; }
+    },
+    console: {
+      onSuccess: function() { process.stdout.write("."); },
+      onFailure: function() { process.stdout.write("F"); },
+      exceptionFormatter: function(ex) { return ex.stack; },
+      onResults: function(passed, failed, failures) {
+        process.stdout.write("\n");
+        if(failed) {
+          process.stdout.write("\n-----------\n");
+          console.log(failures.join("\n"));
+        }
+        
+        var results = [passed + " passed", failed + " failed"];
+        
+        console.log(results.join(", "));
+        
+        if(failed) {
+          throw new Error(failed + " tests failed!");
+        }
+      }
+    }
+  };
+  
   // Helpers
 
   var slice = Array.prototype.slice,
-
+      
       // Shim for Function.prototype.bind
       bind = Function.prototype.bind || function(that) {
         var fn = this;
@@ -20,15 +47,20 @@
         };
       },
 
+      forEach = bind.call(Function.prototype.call, Array.prototype.forEach || function(eachFn, ctx) {
+        ctx = ctx || this;
+        for(var i=0,l=this.length;i<l;i++) {
+          eachFn.call(ctx, this[i], i, this);
+        }
+      }),      
+      
       // Extend a given object with all the properties in passed-in object(s).
       extend = function(obj) {
-        var sources = slice.call(arguments, 1);
-        for(var i=0,l=sources.length,source;i<l;i++) {
-          source = sources[i];
+        forEach(slice.call(arguments, 1), function(source) {
           for(var prop in source) {
             obj[prop] = source[prop];
           }
-        }
+        });
         return obj;
       },
 
@@ -75,7 +107,7 @@
     if(actual.length == 1 && typeof actual[0] == "function") {
       actualFn = actual[0];
     } else {
-      actualFn = function(fn) { return fn.apply(this, actual); }
+      actualFn = function(fn) { return fn.apply(this, actual); };
     }
     
     if(typeof expected !== "function") {
@@ -88,35 +120,49 @@
   });
   
   var given = function(fn) {
-    var steps = [];
+    var steps = [], tested = false;
     
     var wrapper = function() {
+      if(!tested) done(true);
       return fn.apply(this, arguments);
     };
     
-    var addIt = addAssertion(wrapper)(steps);
+    var state = addAssertion(wrapper)(steps);
     
-    wrapper.done = function() {
-      for(var i=0,l=steps.length;i<l;i++) {
-      
-        var stepFn = fn,
-            step = steps[i],
-            context = undefined;
+    var done = wrapper.done = function(immediate) {
+      var eachStep = function(step) {
+        var stepFn = fn, context;
         
-        if(step.ctx) {
-          context = step.ctx()
-          stepFn = bind.call(fn, context);
+        try {
+          if(step.ctx) {
+            context = step.ctx();
+            stepFn = bind.call(fn, context);
+          }
+        
+          var actual = step.actual.call(context, stepFn);
+          step.expected.call(context, options.prepare(actual));
+          options.reporter.onSuccess();
+          passed.push(true);
+        } catch (ex) {
+          failed.push(new Error(ex));
+          options.reporter.onFailure();
         }
-
-        var actual = step.actual.call(context, stepFn);
-        step.expected.call(context, options.prepare(actual));
-      }
+      };
       
+      options.before.call();
+      forEach(steps, options.each(eachStep));
+      options.after.call();
+      
+      
+      if(immediate) results();
+      
+      tested = true;
       return fn;
     };
     
-    wrapper.using = createUsing(addIt);
-    wrapper.when = createWhen(addIt(undefined));
+    wrapper.using = createUsing(state);
+    wrapper.when = createWhen(state(undefined));
+    wrapper.suppose = function() { return wrapper; };
     
     return wrapper; 
   };
@@ -136,34 +182,79 @@
   var createThen = function(state) {
     return function(expected) {
       var result = state(expected);
-      result.and = state
+      result.and = state;
       return result;
-    }
-  }
-  
-  // Defaults
-  function defaultPrepare(actual) {
-    return new DefaultActualWrapper(actual);
-  }
-  
-  function defaultSimpleThen(actual, expected) {
-    actual.equals(expected);
-  }
+    };
+  };
   
   var defaultOptions = {
-      prepare: defaultPrepare,
-      simpleThen: defaultSimpleThen
+    // Defaults
+    prepare: function(actual) { return new DefaultActualWrapper(actual); },
+    simpleThen: function(actual, expected) { actual.equals(expected); },
+    each: function (testcase) { return testcase; },
+    before: function() { /* noop */ },
+    after: function() { /* noop */ },
+    reporter: reporters.console
+  };
+  
+  var createSession = (function() {
+    
+    var each = function(testcase) {
+      var session = this;
+      return function() {
+        try {
+          testcase.apply(this, arguments);
+          session.passed++;
+        } catch(ex) {
+          session.failed.push(ex);
+        }
+        session.total++;
+      };
     };
     
+    var Session = function() {
+      this.total = 0;
+      this.passed = 0;
+      this.failed = [];
+    };
+    
+    Session.prototype.use = function() {
+      return { each: bind(each, this) };
+    };
+    
+    Session.prototype.results = function() { /*noop*/ };
+    
+    return function() {
+      return new Session();
+    };
+  })();
+  
+  var passed = [], failed = [],
+      reset = function() {
+        failed.length = 0;
+        passed.length = 0;
+      },
+      results = function() {
+        var passedCount = passed.length, failedCount = failed.length;
+        var formattedFailures = failed.map(options.reporter.exceptionFormatter);
+        reset();
+        options.reporter.onResults(passedCount, failedCount, formattedFailures);
+      };
+  
+  
   var options = defaultOptions;
   
   def(function() {
     return {
       given: given,
-      use: function(newOptions) {
-        options = extend({}, defaultOptions, newOptions); 
+      use: function() {
+        options = extend.apply(undefined, [{}, defaultOptions].concat(slice.call(arguments))); 
       },
-      version: "0.0.1"
+      reporters: reporters,
+      results: results,
+      reset: reset,
+      createSession: createSession,
+      version: "0.0.2"
     };
   });
 })(this);
